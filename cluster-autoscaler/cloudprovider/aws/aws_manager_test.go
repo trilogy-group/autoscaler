67853e9fe66f04bb49768737757099e5974be244
Lukasz Piatkowski <lukasz.piatkowski@aurea.com>
2019-05-10 11:27:21 +0200

    test cleanups

--- cluster-autoscaler/cloudprovider/aws/aws_manager_test.go
+++ cluster-autoscaler/cloudprovider/aws/aws_manager_test.go
@@ -207,17 +207,15 @@ func TestFetchExplicitAsgs(t *testing.T) {
 		mock.AnythingOfType("func(*autoscaling.DescribeAutoScalingGroupsOutput, bool) bool"),
 	).Run(func(args mock.Arguments) {
 		fn := args.Get(1).(func(*autoscaling.DescribeAutoScalingGroupsOutput, bool) bool)
-		max64 := int64(max - 1)
-		min64 := int64(min)
 		zone := "test-1a"
 		fn(&autoscaling.DescribeAutoScalingGroupsOutput{
 			AutoScalingGroups: []*autoscaling.Group{
 				{
 					AvailabilityZones:    []*string{&zone},
 					AutoScalingGroupName: aws.String(groupname),
-					MaxSize:              &max64,
-					MinSize:              &min64,
-					DesiredCapacity:      &min64,
+					MinSize:              aws.Int64(int64(min)),
+					MaxSize:              aws.Int64(int64(max)),
+					DesiredCapacity:      aws.Int64(int64(min)),
 				},
 			}}, false)
 	}).Return(nil)
